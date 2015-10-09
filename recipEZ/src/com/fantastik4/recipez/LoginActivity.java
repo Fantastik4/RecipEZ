118225b73e14fe75ef8629a40920610d571d7947
Napoleon Fulinara Jr <napoleonfulinarajr@Castle-of-Lions.local>
2015-10-09 01:38:56 -0700

    Fixed double-click bug to log on

--- recipEZ/src/com/fantastik4/recipez/LoginActivity.java
+++ recipEZ/src/com/fantastik4/recipez/LoginActivity.java
@@ -1,51 +1,29 @@
 package com.fantastik4.recipez;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.text.TextUtils;
-import android.util.Log;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
-
 import android.content.Intent;
 import android.widget.EditText;
 import android.view.View.OnClickListener;
 import android.support.v7.app.ActionBarActivity;
 
+@SuppressWarnings("deprecation")
 public class LoginActivity extends ActionBarActivity {
 	Button login;
-	UserProvider userProvider = new UserProvider();
+	UserVerification userVerify = new UserVerification();
 	@Override
 	protected void onCreate(Bundle savedInsanceState) {
 		super.onCreate(savedInsanceState);
 		setContentView(R.layout.activity_main);
 		login = (Button) findViewById(R.id.signIn);
 		login.setClickable(false);
-		new LongRunningGetIO().execute();
-		
-		//****PARSE XML AND RETRIEVE USERNAME AND PASSWORD****
-		
+
 		login.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				//****TESTING VALIDATION ONLY****
-				//****REPLACE TEST DATA WITH PARSED DATA
 
 				EditText usernameCheck = (EditText) findViewById(R.id.username);
 				EditText passwordCheck = (EditText) findViewById(R.id.password);
@@ -53,23 +31,21 @@ public class LoginActivity extends ActionBarActivity {
 				EditText displayErrorMessage = (EditText) findViewById(R.id.error_message);
 
 				if(TextUtils.isEmpty(usernameCheck.getText().toString().trim()) || TextUtils.isEmpty(passwordCheck.getText().toString().trim())) {
-					
+					System.out.println("empty...");
 					//Empty fields
 					displayErrorMessage.setVisibility(View.VISIBLE);
 					displayErrorMessage.setText(getResources().getString(R.string.emptyFields));
-					
+
 				} else {
-					
-					if(ValidateUserCredentials(usernameCheck.getText().toString().trim(), passwordCheck.getText().toString().trim())) {
-						
+					boolean check = ValidateUserCredentials(usernameCheck.getText().toString().trim(), passwordCheck.getText().toString().trim());
+					if(check == true) {
 						//Valid credentials
+						
 						displayErrorMessage.setVisibility(View.INVISIBLE);
 						Intent i = new Intent(LoginActivity.this, SearchActivity.class);
-						
-						//****GO TO PAGE TWO UPON SUCCESS USERNAME AND PASSWORD MATCH****
-						
+
 						startActivity(i);
-						overridePendingTransition(R.animator.animation1, R.animator.animation2);
+						overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
 					} else {
 						//Invalid credentials
 						displayErrorMessage.setVisibility(View.VISIBLE);
@@ -79,69 +55,10 @@ public class LoginActivity extends ActionBarActivity {
 			}
 		});
 	}
-	
+
 	private boolean ValidateUserCredentials(String username, String password)
 	{
-		ArrayList<User> users = userProvider.FetchAllUsers();
-		for(User u: users)
-		{
-			if(u.getName().equals(username) && password.equals("password")) return true;
-		}
-		return false;
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-	
-	private class LongRunningGetIO extends AsyncTask <Void, Void, String> {
-
-		protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
-			InputStream in = entity.getContent();
-			StringBuffer out = new StringBuffer();
-			int n = 1;
-			while (n>0) {
-				byte[] b = new byte[4096];
-				n =  in.read(b);
-				if (n>0) out.append(new String(b, 0, n));
-			}
-			
-			// ****XML STRING****
-			// ****PRINTING OUT FOR TESTING PURPOSES ONLY****
-			System.out.println("OUT TO STRING: " + out.toString());
-			return out.toString();
-		}
-
-		@Override
-		protected String doInBackground(Void... params) {
-			HttpClient httpClient = new DefaultHttpClient();
-			HttpContext localContext = new BasicHttpContext();
-			HttpGet httpGet = new HttpGet("http://recipezservice-recipez.rhcloud.com/rest/users");
-			String text = null;
-			try {
-				HttpResponse response = httpClient.execute(httpGet, localContext);
-				HttpEntity entity = response.getEntity();
-				text = getASCIIContentFromEntity(entity);
-			} catch (Exception e) {
-				return e.getLocalizedMessage();
-			}
-			return text;
-		}
+		boolean valid = userVerify.validate(username, password);
+		return valid;
 	}
 }
---
+++ recipEZ/src/com/fantastik4/recipez/UserVerification.java
@@ -0,0 +1,89 @@
+package com.fantastik4.recipez;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+public class UserVerification {
+	private boolean isValid = false;
+	public UserVerification(){}
+	
+	public synchronized boolean validate(String u, String p){
+		try {
+			ValidateUser(u, p);
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("FIRST: " + isValid);
+		return isValid;
+	}
+
+	public void ValidateUser (final String name, final String pass) {
+		Thread thread = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				try {
+					XmlPullParserFactory xmlFactoryObject;
+					URL url = new URL("http://recipezservice-recipez.rhcloud.com/rest/VerificationServices/VerifyUser/"+name+"/"+pass);
+					HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+					conn.setReadTimeout(10000 /* milliseconds */);
+					conn.setConnectTimeout(15000 /* milliseconds */);
+					conn.setRequestMethod("GET");
+					conn.setDoInput(true);
+					conn.connect();
+
+					InputStream stream = conn.getInputStream();
+
+					xmlFactoryObject = XmlPullParserFactory.newInstance();
+					XmlPullParser myParser = xmlFactoryObject.newPullParser();
+
+					myParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+					myParser.setInput(stream, null);
+					ParseVerification(myParser);
+					System.out.println("PARSEVARIFY: " + isValid);
+					stream.close();
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
+	}
+
+	public void ParseVerification(XmlPullParser myParser) 
+	{
+		int event;
+		String value = "", text = "";
+		try {
+			event = myParser.getEventType();
+			while (event != XmlPullParser.END_DOCUMENT) {
+				String name=myParser.getName();
+				switch (event){
+				case XmlPullParser.START_TAG:
+					if(name.equals("verificationResponse")){
+						if(myParser.next() == XmlPullParser.TEXT) 
+						{ 
+							text = myParser.getText();
+						}
+						value = text;
+					}
+					break;
+				case XmlPullParser.END_TAG:
+					if(value.equals("true")){
+						isValid = true;
+					}
+				}
+				event = myParser.next();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
