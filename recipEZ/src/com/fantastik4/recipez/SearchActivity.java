10c72fe71d75f9798a75e1205755b8327fb2bf7e
Brian <bchan930@gmail.com>
2015-10-14 11:35:12 -0700

    update

--- recipEZ/src/com/fantastik4/recipez/SearchActivity.java
+++ recipEZ/src/com/fantastik4/recipez/SearchActivity.java
@@ -8,57 +8,76 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 
 public class SearchActivity extends Activity {
-	RecipeProvider recipeProvider = new RecipeProvider();
-	ArrayList<Recipe> recipes;
+	IngredientProvider ingredientProvider;
+	ArrayList<Ingredient> ingredients;
+	ArrayList<Boolean> checkedStates;
 	Button search;
 	ListView displayRecipes;
-	ArrayList<String> recipeNames;
-	ArrayAdapter<String> itemsAdapter;
+	ArrayList<String> ingredientNames;
 	SearchActivity sa = this;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
 		
-		recipeProvider = new RecipeProvider();
-		displayRecipes = (ListView) findViewById(R.id.recipeList);
-
+		ingredientProvider = new IngredientProvider();
+		checkedStates = new ArrayList<Boolean>();
+		
 		search = (Button)findViewById(R.id.findFood);
 		search.setClickable(false);
 		
-		itemsAdapter = new ArrayAdapter<String>(
-				this, 
-				android.R.layout.simple_list_item_1,
-				recipeNames );
-		
+		PopulateIngredientList();
 		
 		search.setOnClickListener(new OnClickListener(){
 			public void onClick(View v) 
 			{
-				recipes = recipeProvider.FetchAllRecipes();
-				ListView lv = (ListView)findViewById(R.id.recipeList);
 				
-                ArrayList<String> recipeNames = new ArrayList<String>();
-                for(Recipe r: recipes)
-                {
-                    recipeNames.add(r.getName());
-                }
-                
-                 ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                         sa, 
-                         android.R.layout.simple_list_item_1,
-                         recipeNames );
-
-                 lv.setAdapter(arrayAdapter);
 			}
 		});
+		
+		
 	}
 
+	private void PopulateIngredientList()
+	{
+		ingredients = ingredientProvider.FetchAllIngredients();
+		ListView lv = (ListView)findViewById(R.id.recipeList);
+		
+        ArrayList<String> ingredientNames = new ArrayList<String>();
+        for(Ingredient i: ingredients)
+        {
+           ingredientNames.add(i.getName());
+           checkedStates.add(false);
+        }
+        
+         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                 sa, 
+                 android.R.layout.simple_list_item_multiple_choice,
+                 ingredientNames );
+
+         lv.setAdapter(arrayAdapter);
+         
+         lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+         lv.setOnItemClickListener(new OnItemClickListener(){
+        	 public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        	 {
+        		 boolean currentlyChecked = checkedStates.get(position);
+                 checkedStates.set(position, !currentlyChecked);
+        		 
+                 CheckedTextView markedItem = (CheckedTextView) view;
+                 markedItem.setChecked(checkedStates.get(position));
+        	 }
+         });
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
