4a2483aa044fe75ca41052c9c3fc691361100036
Napoleon Fulinara Jr <nfulinarajr@users.noreply.github.com>
2015-12-08 14:07:11 -0800

    Fixed bug when searching from a null array

--- recipEZ/src/activities/EditListActivity.java
+++ recipEZ/src/activities/EditListActivity.java
@@ -14,7 +14,6 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import com.fantastik4.recipez.R;
 
-
 import android.widget.SearchView;
 import resources.IngredientProvider;
 import resources.RecipeResourceProvider;
@@ -22,7 +21,6 @@ import objects.CheckedListViewAdapter;
 
 public class EditListActivity extends Activity {
 
-	
 	String userID;
 	ListView listView;
 	SearchView searchField;
@@ -31,72 +29,71 @@ public class EditListActivity extends Activity {
 	IngredientProvider ingredientProvider;
 	CheckedListViewAdapter ingredientListAdapter;
 	ArrayList<Model> displayedIngredients;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_list);
-		
+
 		userID = (String) getIntent().getSerializableExtra("CurrentUsername");
 		ingredientProvider = new IngredientProvider();
 		PopulateIngredientList();
 	}
-	
+
 	private void PopulateIngredientList() {
-		listView = (ListView)findViewById(R.id.lv_userList);
+		listView = (ListView) findViewById(R.id.lv_userList);
 		findRecipes = (ImageView) findViewById(R.id.edit_list_get_recipes);
 		trash = (ImageView) findViewById(R.id.edit_list_trash);
-		
+
 		ingredients = ingredientProvider.FetchIngredientsByUserID(userID);
 		displayedIngredients = ConvertIngredientsToModelArray(ingredients);
 		ingredientListAdapter = new CheckedListViewAdapter(this, displayedIngredients);
 
 		listView.setAdapter(ingredientListAdapter);
 		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-		
-		trash.setOnClickListener(new OnClickListener(){
+
+		trash.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View trashView) 
-			{
-				for(int i = 0; i < displayedIngredients.size(); i++)
-				{
-					if(displayedIngredients.get(i).isSelected())
-						{
-							ingredientProvider.RemoveIngredientFromUser(userID, ingredients.get(i).getIngredientID());
-						}
+			public void onClick(View trashView) {
+				for (int i = 0; i < displayedIngredients.size(); i++) {
+					if (displayedIngredients.get(i).isSelected()) {
+						ingredientProvider.RemoveIngredientFromUser(userID, ingredients.get(i).getIngredientID());
+					}
 				}
 				PopulateIngredientList();
-			}			
+			}
 		});
-		
-		findRecipes.setOnClickListener(new OnClickListener(){
+
+		findRecipes.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				ArrayList<Recipe> recipeResults = GetRecipeResults();
-				Intent i = new Intent(EditListActivity.this, RecipeResultsActivity.class);
-				i.putExtra("RecipeResults", recipeResults);
-				i.putExtra("username", userID);
-				startActivity(i);
 
-				overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+				if (recipeResults != null) {
+					Intent i = new Intent(EditListActivity.this, RecipeResultsActivity.class);
+					i.putExtra("RecipeResults", recipeResults);
+					i.putExtra("username", userID);
+					startActivity(i);
+
+					overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+				}
 			}
-			
 		});
 	}
-	
+
 	private ArrayList<Recipe> GetRecipeResults() {
 		RecipeResourceProvider recipeProvider = new RecipeResourceProvider();
 		ArrayList<Recipe> recipes = new ArrayList<Recipe>();
 		ArrayList<Recipe> recipeResults = null;
-		for(int i = 0; i < displayedIngredients.size(); i++)
-		{
-			if(displayedIngredients.get(i).isSelected()) 
-			{
+		for (int i = 0; i < displayedIngredients.size(); i++) {
+			if (displayedIngredients.get(i).isSelected()) {
 				recipes = recipeProvider.FetchRecipesByIngredientID(ingredients.get(i).getIngredientID());
-				if(recipeResults == null) recipeResults = recipes;
-				else recipeResults = IntersectionOfRecipeLists(recipeResults, recipes);
+				if (recipeResults == null)
+					recipeResults = recipes;
+				else
+					recipeResults = IntersectionOfRecipeLists(recipeResults, recipes);
 			}
 		}
 		return recipeResults;
@@ -106,7 +103,7 @@ public class EditListActivity extends Activity {
 		ArrayList<Recipe> list = new ArrayList<Recipe>();
 
 		for (Recipe r : list1) {
-			if(ListContainsRecipe(list2, r)) {
+			if (ListContainsRecipe(list2, r)) {
 				list.add(r);
 			}
 		}
@@ -115,9 +112,9 @@ public class EditListActivity extends Activity {
 	}
 
 	private boolean ListContainsRecipe(ArrayList<Recipe> list2, Recipe recipe) {
-		for(Recipe r: list2)
-		{
-			if(r.getRecipeID().equals(recipe.getRecipeID()))return true;
+		for (Recipe r : list2) {
+			if (r.getRecipeID().equals(recipe.getRecipeID()))
+				return true;
 		}
 		return false;
 	}
@@ -125,7 +122,7 @@ public class EditListActivity extends Activity {
 	private ArrayList<Model> ConvertIngredientsToModelArray(ArrayList<Ingredient> ingredients) {
 		ArrayList<Model> ingredientNames = new ArrayList<Model>();
 		Model temp;
-		for(Ingredient i: ingredients) {
+		for (Ingredient i : ingredients) {
 			temp = new Model(i.getName());
 			temp.setSelected(false);
 			ingredientNames.add(temp);
--- recipEZ/src/activities/IngredientSearchActivity.java
+++ recipEZ/src/activities/IngredientSearchActivity.java
@@ -39,20 +39,19 @@ public class IngredientSearchActivity extends Activity {
 		setContentView(R.layout.activity_search);
 
 		/**
-		 * LABLES:	Search Text Field - sv_searchIngredients
-		 * 			Ingredients List - lv_ingredientsList
+		 * LABLES: Search Text Field - sv_searchIngredients Ingredients List -
+		 * lv_ingredientsList
 		 */
 		ingredientProvider = new IngredientProvider();
 		currentUsername = (String) getIntent().getSerializableExtra("CurrentUsername");
 
-		//Initializing SearchField On Query Text Listener
+		// Initializing SearchField On Query Text Listener
 		searchField = (SearchView) findViewById(R.id.sv_searchIngredients);
 		searchField.setSelected(true);
-		searchField.setOnQueryTextListener(new OnQueryTextListener(){
-			
+		searchField.setOnQueryTextListener(new OnQueryTextListener() {
+
 			@Override
-			public boolean onQueryTextChange(String searchArg) 
-			{
+			public boolean onQueryTextChange(String searchArg) {
 				UpdateDisplayedIngredients(searchArg);
 				ingredientListAdapter.notifyDataSetChanged();
 				return false;
@@ -65,36 +64,33 @@ public class IngredientSearchActivity extends Activity {
 			}
 		});
 
-		
-		//Initializing Buttons
+		// Initializing Buttons
 		findRecipes = (ImageView) findViewById(R.id.edit_list_get_recipes);
-		findRecipes.setOnClickListener(new OnClickListener(){
-		
-		
-		
+		findRecipes.setOnClickListener(new OnClickListener() {
+
 			public void onClick(View arg0) {
 				ArrayList<Recipe> recipeResults = GetRecipeResults();
-				Intent i = new Intent(IngredientSearchActivity.this, RecipeResultsActivity.class);
-				i.putExtra("RecipeResults", recipeResults);
-				i.putExtra("username", currentUsername);
-				startActivity(i);
+				if (recipeResults != null) {
+					Intent i = new Intent(IngredientSearchActivity.this, RecipeResultsActivity.class);
+					i.putExtra("RecipeResults", recipeResults);
+					i.putExtra("username", currentUsername);
+					startActivity(i);
 
-				overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+					overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+				}
 			}
 		});
 
 		addToFridge = (ImageView) findViewById(R.id.AddToFridgeButton);
-		addToFridge.setOnClickListener(new OnClickListener(){
-			public void onClick(View view)
-			{
+		addToFridge.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
 				AddIngredientsToFridge();
 			}
 		});
-		
+
 		reset = (ImageView) findViewById(R.id.ingredientListResetButton);
-		reset.setOnClickListener(new OnClickListener(){
-			public void onClick(View view)
-			{
+		reset.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
 				ResetIngredientList();
 				UpdateDisplayedIngredients("");
 				ingredientListAdapter.notifyDataSetChanged();
@@ -102,21 +98,16 @@ public class IngredientSearchActivity extends Activity {
 		});
 		PopulateIngredientList();
 	}
-	
-	public void ResetIngredientList()
-	{
-		for(Model m: ingredientNames)
-		{
+
+	public void ResetIngredientList() {
+		for (Model m : ingredientNames) {
 			m.setSelected(false);
 		}
 	}
 
-	private void AddIngredientsToFridge()
-	{
-		for(int i = 0; i < ingredientNames.size(); i++)
-		{
-			if(ingredientNames.get(i).isSelected())
-			{
+	private void AddIngredientsToFridge() {
+		for (int i = 0; i < ingredientNames.size(); i++) {
+			if (ingredientNames.get(i).isSelected()) {
 				ingredientProvider.AddIngredientToUser(currentUsername, ingredients.get(i).getIngredientID());
 			}
 		}
@@ -126,13 +117,13 @@ public class IngredientSearchActivity extends Activity {
 		RecipeResourceProvider recipeProvider = new RecipeResourceProvider();
 		ArrayList<Recipe> recipes = new ArrayList<Recipe>();
 		ArrayList<Recipe> recipeResults = null;
-		for(int i = 0; i < ingredientNames.size(); i++)
-		{
-			if(ingredientNames.get(i).isSelected()) 
-			{
+		for (int i = 0; i < ingredientNames.size(); i++) {
+			if (ingredientNames.get(i).isSelected()) {
 				recipes = recipeProvider.FetchRecipesByIngredientID(ingredients.get(i).getIngredientID());
-				if(recipeResults == null) recipeResults = recipes;
-				else recipeResults = IntersectionOfRecipeLists(recipeResults, recipes);
+				if (recipeResults == null)
+					recipeResults = recipes;
+				else
+					recipeResults = IntersectionOfRecipeLists(recipeResults, recipes);
 			}
 		}
 		return recipeResults;
@@ -142,7 +133,7 @@ public class IngredientSearchActivity extends Activity {
 		ArrayList<Recipe> list = new ArrayList<Recipe>();
 
 		for (Recipe r : list1) {
-			if(ListContainsRecipe(list2, r)) {
+			if (ListContainsRecipe(list2, r)) {
 				list.add(r);
 			}
 		}
@@ -151,56 +142,52 @@ public class IngredientSearchActivity extends Activity {
 	}
 
 	private boolean ListContainsRecipe(ArrayList<Recipe> list2, Recipe recipe) {
-		for(Recipe r: list2)
-		{
-			if(r.getRecipeID().equals(recipe.getRecipeID()))return true;
+		for (Recipe r : list2) {
+			if (r.getRecipeID().equals(recipe.getRecipeID()))
+				return true;
 		}
 		return false;
 	}
 
 	@SuppressLint("DefaultLocale")
-	private void UpdateDisplayedIngredients(String searchArg) 
-	{
+	private void UpdateDisplayedIngredients(String searchArg) {
 		displayedIngredients.clear();
 		AddSelectedIngredientsToDisplayList();
-		if(searchArg.length() <= 0) return;
+		if (searchArg.length() <= 0)
+			return;
 
-		for(Model m: ingredientNames)
-		{
-			if(m.getName().toLowerCase().contains(searchArg.toLowerCase()))
-			{
-				if(!ModelListContainsString(displayedIngredients, m.getName())) displayedIngredients.add(m);
+		for (Model m : ingredientNames) {
+			if (m.getName().toLowerCase().contains(searchArg.toLowerCase())) {
+				if (!ModelListContainsString(displayedIngredients, m.getName()))
+					displayedIngredients.add(m);
 			}
 		}
 	}
-	
-	private void AddSelectedIngredientsToDisplayList()
-	{
-		for(Model m: ingredientNames) 
-		{
-			if(m.isSelected()) displayedIngredients.add(m);
+
+	private void AddSelectedIngredientsToDisplayList() {
+		for (Model m : ingredientNames) {
+			if (m.isSelected())
+				displayedIngredients.add(m);
 		}
 	}
-	
-	private boolean ModelListContainsString(ArrayList<Model> list, String name)
-	{
-		for(Model m: list)
-		{
-			if(m.getName().equals(name)) return true;
+
+	private boolean ModelListContainsString(ArrayList<Model> list, String name) {
+		for (Model m : list) {
+			if (m.getName().equals(name))
+				return true;
 		}
 		return false;
 	}
 
 	@SuppressWarnings("unchecked")
-	private void PopulateIngredientList() 
-	{
-		listView = (ListView)findViewById(R.id.lv_ingredientsList);		
+	private void PopulateIngredientList() {
+		listView = (ListView) findViewById(R.id.lv_ingredientsList);
 
 		ingredients = ingredientProvider.FetchAllIngredients();
 		ingredientNames = ConvertIngredientsToModelArray(ingredients);
 		displayedIngredients = new ArrayList<Model>();
 		AddSelectedIngredientsToDisplayList();
-		
+
 		ingredientListAdapter = new CheckedListViewAdapter(this, displayedIngredients);
 
 		listView.setAdapter(ingredientListAdapter);
@@ -213,7 +200,7 @@ public class IngredientSearchActivity extends Activity {
 	private ArrayList<Model> ConvertIngredientsToModelArray(ArrayList<Ingredient> ingredients) {
 		ArrayList<Model> ingredientNames = new ArrayList<Model>();
 		Model temp;
-		for(Ingredient i: ingredients) {
+		for (Ingredient i : ingredients) {
 			temp = new Model(i.getName());
 			temp.setSelected(false);
 			ingredientNames.add(temp);
